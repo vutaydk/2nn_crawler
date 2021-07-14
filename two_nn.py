3e4bb4dc9d2d7fb192f74a80b293ceb932481de4
vupb <[omitted]>
2021-07-15 01:22:38 +0700

    remove 403

--- two_nn.py
+++ two_nn.py
@@ -18,7 +18,6 @@ DESKTOP_AGENTS = itertools.cycle([
     {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393'},
     {'User-Agent': 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)'},
     {'User-Agent': 'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0; .NET CLR 1.1.4322; .NET CLR 2.0.50727; .NET CLR 3.0.4506.2152; .NET CLR 3.5.30729)'},
-    {'User-Agent': 'Mozilla/5.0 (compatible, MSIE 11, Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko'}
 ])
 
 logging.basicConfig(
