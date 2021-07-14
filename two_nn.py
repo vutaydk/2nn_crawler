4e76112e239c6e1e13c6c49866edf68b42c6c9cf
nndat <nguyenngocdat90@gmail.com>
2021-07-14 23:01:25 +0700

    fix parse 5ch content

--- two_nn.py
+++ two_nn.py
@@ -1,60 +1,68 @@
-from datetime import datetime
+import os
+import json
 import logging
-from collections import defaultdict
 import requests
+
+from collections import defaultdict
 from bs4 import BeautifulSoup
 
-logging.basicConfig(filename="std.log", 
-					format='%(asctime)s %(message)s', 
-					filemode='w')
-logger=logging.getLogger() 
-logger.setLevel(logging.DEBUG) 
 
+LIMIT_LAST_100_COMMENTS = "/-100"
 TWONN_URL = "https://www.2nn.jp/matsuri/s{}.html"
 POST_PER_PAGE = 50
 
+
+logging.basicConfig(
+    filename="std.log",
+    format="%(asctime)s %(message)s",
+    filemode="w"
+)
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+
 def crawl(page):
-        logger.info(f"Start crawl {page}~{page+POST_PER_PAGE} article")
-        articles_in_page = defaultdict(list)
+    logger.info(f"Start crawl {page}~{page+POST_PER_PAGE} article")
+    articles_in_page = defaultdict(list)
 
-        url = TWONN_URL.format(page)
-        res = requests.get(url)
-        if res.status_code !=200:
-            logger.warning(f"fail: {url}")
-            return
-        
-        soup = BeautifulSoup(res.text, "html5lib")
-        articles_wrapper = soup.select(".news4plus article ol li")
-        i =0 
-        for article in articles_wrapper:
-            i +=1
-            if i == 2:
-                break
-            pubtime_dom = article.find("time")
-            pubdatetime = pubtime_dom["datetime"]
-            pubdate = pubdatetime.split(" ")[0]
+    url = TWONN_URL.format(page)
+    res = requests.get(url)
+    if res.status_code != 200:
+        logger.warning(f"fail: {url}")
+        return
 
-            title_dom = article.select("h2>a")
-            title = title_dom[0].text
-            article_url = title_dom[0]["href"]
-            article_content, article_response = get_5ch_content(article_url)
+    soup = BeautifulSoup(res.text, "html5lib")
+    articles_wrapper = soup.select(".news4plus article ol li")
+    i = 0
+    for article in articles_wrapper:
+        i += 1
+        if i == 2:
+            break
+        pubtime_dom = article.find("time")
+        pubdatetime = pubtime_dom["datetime"]
+        pubdate = pubdatetime.split(" ")[0]
 
-            if not article and not article_response:
-                continue
-            
-            out = {
-                "pubdatetime": pubdatetime,
-                "url": article_url,
-                "title": title,
-                "content": article_content,
-                "response": article_response
-            }
-            articles_in_page[pubdate].append(out)
-        logger.info(f"Finish Crawl {page}~{page+POST_PER_PAGE} article")
-        save(articles_in_page)
+        title_dom = article.select("h2>a")
+        title = title_dom[0].text
+        article_url = title_dom[0]["href"]
+        article_content, article_response = get_5ch_content(article_url)
 
-import os
-import json
+        if not article and not article_response:
+            continue
+
+        out = {
+            "pubdatetime": pubdatetime,
+            "url": article_url,
+            "title": title,
+            "content": article_content,
+            "response": article_response,
+        }
+        articles_in_page[pubdate].append(out)
+    logger.info(f"Finish Crawl {page}~{page+POST_PER_PAGE} article")
+    save(articles_in_page)
+
+
 def save(articles_grouped_by_date):
     logger.info("Save to file")
     data_dir = "data"
@@ -68,13 +76,10 @@ def save(articles_grouped_by_date):
     logger.info("Saving done")
 
 
-
-    
-LIMIT_LAST_100_COMMENTS = "/-100"
 def get_5ch_content(url):
-    #show all without paging
+    # show all without paging
     if url.endswith(LIMIT_LAST_100_COMMENTS):
-        url.replace(LIMIT_LAST_100_COMMENTS, "")
+        url = url.replace(LIMIT_LAST_100_COMMENTS, "")
 
     res = requests.get(url)
     if res.status_code != 200:
@@ -82,13 +87,26 @@ def get_5ch_content(url):
         return None, None
 
     soup = BeautifulSoup(res.text, "html5lib")
+
     postes = soup.select(".post .message")
+    if not postes:
+        postes = soup.select("dl.thread dd")
+
     postes_text = [post.text for post in postes]
-    return postes_text[0], postes_text[1:]
+
+    if not postes_text:
+        logger.warning(f"Can't get posts from {url}")
+        return None, None
+
+    content = postes_text[0]
+    responses = postes_text[1:] if len(postes_text) > 1 else []
+
+    return content, responses
+
 
 if __name__ == "__main__":
     logger.info("Start cawling")
-    for page in range(1, 30000+1, POST_PER_PAGE):
+    for page in range(1, 30000 + 1, POST_PER_PAGE):
         try:
             crawl(page)
         except Exception as e:
