ccbf2eea543e3b7c27af08d1460b835532642572
vupb <[omitted]>
2021-07-15 01:17:26 +0700

    use user-agent to avoid block too many request

--- two_nn.py
+++ two_nn.py
@@ -2,6 +2,7 @@ import os
 import json
 import logging
 import requests
+import itertools
 
 from collections import defaultdict
 from bs4 import BeautifulSoup
@@ -11,6 +12,14 @@ LIMIT_LAST_100_COMMENTS = "/-100"
 TWONN_URL = "https://www.2nn.jp/matsuri/s{}.html"
 POST_PER_PAGE = 50
 
+DESKTOP_AGENTS = itertools.cycle([
+    {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'},
+    {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:53.0) Gecko/20100101 Firefox/53.0'},
+    {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393'},
+    {'User-Agent': 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)'},
+    {'User-Agent': 'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0; .NET CLR 1.1.4322; .NET CLR 2.0.50727; .NET CLR 3.0.4506.2152; .NET CLR 3.5.30729)'},
+    {'User-Agent': 'Mozilla/5.0 (compatible, MSIE 11, Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko'}
+])
 
 logging.basicConfig(
     filename="std.log",
@@ -77,7 +86,7 @@ def get_5ch_content(url):
     if url.endswith(LIMIT_LAST_100_COMMENTS):
         url = url.replace(LIMIT_LAST_100_COMMENTS, "")
 
-    res = requests.get(url)
+    res = requests.get(url, headers=DESKTOP_AGENTS.__next__())
     if res.status_code != 200:
         logger.warning(f"fail open 5chn: {url}")
         return None, None
